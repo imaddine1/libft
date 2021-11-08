5008d17ae274c927738cf1f05cd90230e047027f
imaddine1 <hh.imad2016@gmail.com>
2021-11-08 10:39:16 +0100

    add space for null terminator

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,36 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: iharile <iharile@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/08 07:51:40 by iharile           #+#    #+#             */
-/*   Updated: 2021/11/08 10:38:22 by iharile          ###   ########.fr       */
+/*   Created: 2021/11/08 10:22:16 by iharile           #+#    #+#             */
+/*   Updated: 2021/11/08 10:24:28 by iharile          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char *ft_strjoin(char const *s1, char const *s2)
 {
-	char	*str;
-	size_t	i;
-	char	*s2;
-	size_t	j;
-
-	j = 0;
-	i = (size_t)start;
-	str = (char *)s;
-	s2 = malloc ((len + 1) * sizeof(char));
-	if (s2 == NULL)
-		return (0);
-	while (str[i] && j < len)
-	{
-		s2[j] = str[i];
-		j++;
-		i++;
-	}
-	s2[j] = '\0';
-	return (s2);
+	
 }
