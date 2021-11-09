adff4bd1bada89be3979b979b7a6731dde0e07f8
imaddine1 <hh.imad2016@gmail.com>
2021-11-09 10:46:50 +0100

    handle ft_atoi like official

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,35 +6,56 @@
 /*   By: iharile <iharile@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 10:24:23 by iharile           #+#    #+#             */
-/*   Updated: 2021/11/06 14:13:45 by iharile          ###   ########.fr       */
+/*   Updated: 2021/11/09 10:45:42 by iharile          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <limits.h>
+
+int	check_sign(char	*str)
+{
+	int	i;
+
+	i = 0;
+	if (str[i] == '+' || str[i] == '-')
+	{	
+		if (str[i] == '-')
+			return (-1);
+		else
+			return (1);
+	}
+	return (0);
+}
+
+int	check_res(int s)
+{
+	if (s < 0)
+		return (0);
+	return (-1);
+}
 
 int	ft_atoi(const char *str)
 {
-	char	*s;
-	int		res;
-	int		sign;
-	int		i;
+	long int	res;
+	int			sign;
 
-	i = 0;
 	res = 0;
 	sign = 1;
-	s = (char *)str;
-	while ((s[i] >= 9 && s[i] <= 13) || s[i] == 32)
-		i++;
-	if (s[i] == '+' || s[i] == '-')
+	while ((*str >= 9 && *str <= 13) || *str == 32)
+		str++;
+	if (check_sign((char *)str) != 0)
 	{
-		if (s[i] == '-')
-			sign *= -1;
-		i++;
+		sign *= check_sign((char *)str);
+		str++;
 	}
-	while (s[i] >= '0' && s[i] <= '9')
+	while (*str >= '0' && *str <= '9')
 	{
-		res = res * 10 + s[i] - '0';
-		i++;
-	}
+		res = res * 10 + *str - '0';
+		if (res <= 2147483648)
+			str++;
+		else
+			return (check_res(sign));
+	}	
 	return (res * sign);
 }
