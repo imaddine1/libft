9befbd61e6b1f3a1b9a77d3140734d882bb5df4b
imaddine1 <hh.imad2016@gmail.com>
2021-11-17 15:58:15 +0100

    lstiter is done

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: iharile <iharile@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 10:18:44 by iharile           #+#    #+#             */
-/*   Updated: 2021/11/17 12:53:52 by iharile          ###   ########.fr       */
+/*   Updated: 2021/11/17 15:44:31 by iharile          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,4 +66,5 @@ t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
 void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
 #endif
